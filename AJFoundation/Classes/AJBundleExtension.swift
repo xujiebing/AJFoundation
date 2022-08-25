647c56e7ad5e80a0d1c3b137a8f12c5911327401
shanying.xu <shanying.xu@tuya.com>
2022-08-25 10:17:18 +0800

    feat: add code

--- AJFoundation/Classes/AJBundleExtension.swift
+++ AJFoundation/Classes/AJBundleExtension.swift
@@ -13,4 +13,12 @@ public extension Bundle {
         let result = try String.init(contentsOfFile: path, encoding: String.Encoding.utf8)
         return result
     }
+    
+    func ajGetJsonString(resource: String, type: String) throws -> String? {
+        guard let path = self.path(forResource: resource, ofType: type) else {
+            return nil
+        }
+        let result = try String.init(contentsOfFile: path, encoding: String.Encoding.utf8)
+        return result
+    }
 }
---
+++ AJFoundation/Classes/AJObject.swift
@@ -0,0 +1,12 @@
+//
+//  AJObject.swift
+//  CGDataCenterModule
+//
+//  Created by 山鹰 on 2022/8/25.
+//
+
+import Foundation
+
+open class AJObject: NSObject {
+    
+}
