5496bdd737c10ab7b13050c265d83ba3cd615035
shanying <shanying@icheego.com>
2023-01-06 13:45:04 +0800

    添加时间戳方法

--- AJFoundation/Classes/DateExtension.swift
+++ AJFoundation/Classes/DateExtension.swift
@@ -9,5 +9,18 @@ import Foundation
 
 public extension Date {
     
-    
+    /// 获取当前 秒级 时间戳 - 10位
+    var timeStampSec: Int {
+        let timeInterval: TimeInterval = self.timeIntervalSince1970
+        let timeStamp = Int(timeInterval)
+        return timeStamp
+    }
+
+    /// 获取当前 毫秒级 时间戳 - 13位
+    var timeStampMSec: Int {
+        let timeInterval: TimeInterval = self.timeIntervalSince1970
+        let millisecond = Int(timeInterval*1000)
+        return millisecond
+    }
+
 }
